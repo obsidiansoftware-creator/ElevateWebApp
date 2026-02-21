f6b4d26a331132613a9ab4346708578e077bdee8
obsidiansoftware <obsidiansoftwarecontact@gmail.com>
2026-02-20 21:58:50 -0600

    Actualizar db.js

--- elevate-manager/lib/db.js
+++ elevate-manager/lib/db.js
@@ -2,10 +2,10 @@
 import mysql from 'mysql2/promise';
 
 export const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: tramway.proxy.rlwy.net:37245,
+  user: root,
+  password: BgzwrpehmYEcLwqHYnoRLrKzkGaMDAyk,
+  database: railway,
   waitForConnections: true,
   connectionLimit: 10,
 });
